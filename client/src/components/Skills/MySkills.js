0134dfdc47851ac0090056c0c04fd385e8681126
Nhours <alapideseb@hotmail.fr>
2023-09-27 08:58:53 +0200

    Widht & height à70px à la place de 50px

--- client/src/components/Skills/MySkills.js
+++ client/src/components/Skills/MySkills.js
@@ -158,11 +158,11 @@ const MySkills = ({ IsInLogin }) => {
                 </form>
               ) : (
                   <>
-                    <IconContainer style={{ fontSize: '0.5rem' }} color="blue">
+                    <IconContainer style={{ fontSize: '1rem' }} color="blue">
                   <img
                     src={skill.icon} // Utilisez l'URL de l'image stockée dans la base de données
                     alt={skill.tech} // Utilisez le nom de la compétence comme texte alternatif
-                    style={{ width: '50px', height: '50px' }} // Ajustez la taille de l'image selon vos besoins
+                    style={{ width: '70px', height: '70px' }} // Ajustez la taille de l'image selon vos besoins
                       />
                       </IconContainer>
                   <Heading as="h4" size="h4">
