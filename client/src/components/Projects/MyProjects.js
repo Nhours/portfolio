f7f40840b8935d1f59f6f448bf501b621eeb3bfa
Nhours <alapideseb@hotmail.fr>
2023-09-27 10:52:01 +0200

    Modal connect showcase

--- client/src/components/Projects/MyProjects.js
+++ client/src/components/Projects/MyProjects.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FaGithub } from 'react-icons/fa';
-import Modal from 'react-modal'
 
 import {
     PaddingContainer,
